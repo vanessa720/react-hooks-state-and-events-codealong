a2feeebcd3cfcc3628f88f3fe9b7063acdf32c94
Vanessa720 <vanessawilliamson718@gmail.com>
2021-05-15 14:22:10 -0400

    Completed Assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
-
 function Toggle() {
-  return <button>OFF</button>;
-}
+  const [isOn, setIsOn] = useState(false);
 
-export default Toggle;
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+} 
